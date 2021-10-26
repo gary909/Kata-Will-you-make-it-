ac4c209e54081331ec9b98c87a995f8fbc86182c
gary <gary.white@lodgit.com>
2021-10-26 13:01:40 +0200

    code working

--- kataMakeIt.js
+++ kataMakeIt.js
@@ -1,5 +1,10 @@
 const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     // TODO
+    if (fuelLeft * mpg >= distanceToPump) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 console.log(zeroFuel(50, 25, 2)); // Return True
